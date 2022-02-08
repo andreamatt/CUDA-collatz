3b3c1902557bd8f456f34e3a82e423f4602ff013
andreamatt <andreamatte97@gmail.com>
2022-02-08 21:03:47 +0100

    Better visualization

--- visualize.py
+++ visualize.py
@@ -4,25 +4,26 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 point_size = 0.3
-AVG = np.fromfile("output/avg.bin", dtype=np.uint16)[:10000]
-MIN = np.fromfile("output/min.bin", dtype=np.uint16)[:10000]
-MAX = np.fromfile("output/max.bin", dtype=np.uint16)[:10000]
-X = np.arange(len(AVG))
+stride = 2**10
+AVG = np.fromfile("output/avg.bin", dtype=np.uint16)[0:-1:stride]
+MIN = np.fromfile("output/min.bin", dtype=np.uint16)[0:-1:stride]
+MAX = np.fromfile("output/max.bin", dtype=np.uint16)[0:-1:stride]
+X = np.arange(0, len(AVG) * stride, stride)
 data = pd.DataFrame({"Average": AVG, "Min": MIN, "Max": MAX, "Batch": X})
 
 # plot avg using seaborn
 sns.scatterplot(data=data, x="Batch", y="Average", s=point_size)
-plt.savefig("images/avg.png")
+plt.savefig("images/avg.png", dpi=300)
 plt.clf()
 
 # plot min
 sns.scatterplot(data=data, x="Batch", y="Min", s=point_size)
-plt.savefig("images/min.png")
+plt.savefig("images/min.png", dpi=300)
 plt.clf()
 
 # plot max
 sns.scatterplot(data=data, x="Batch", y="Max", s=point_size)
-plt.savefig("images/max.png")
+plt.savefig("images/max.png", dpi=300)
 plt.clf()
 
 # plot all three
@@ -34,4 +35,4 @@ all_df = pd.DataFrame(
     }
 )
 sns.scatterplot(data=all_df, x="Batch", y="Steps", hue="Metric", s=point_size)
-plt.savefig("images/all.png")
+plt.savefig("images/all.png", dpi=300)
